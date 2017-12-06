f85b9920c8a859acc5a6e18d7d2d7120f1136cf4
jaop2316 <jessikita.ldu@gmail.com>
2017-12-06 16:56:50 -0500

    Change navbar color and routes displays

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,7 +31,57 @@ $(document).ready(function () {
         doScroll(this)
     });*/
 
-    $('.lead .btn-primary').each(function(){
+    $('.lead .btn-primary').each(function () {
         doScroll(this)
     });
+
+
+    /*
+    * Change a navbar color to secondary screens
+    * */
+    var ready = function () {
+        if ($('.main-banner').length != 1) {
+            console.log("no tiene main banner");
+            $('header').addClass('header-white');
+            $('header .nav-pills').addClass('blue-links');
+        }
+
+        $('.stations-detail-page img').addClass('img-responsive');
+    };
+
+    $(document).on('turbolinks:load', ready);
+
+    /**
+     * Listen to scroll to change header opacity class
+     */
+    function checkScroll() {
+        var startY = $('header .nav-pills').height() * 2; //The point where the navbar changes in px
+
+        if ($(window).scrollTop() > startY) {
+            $('header').addClass('header-white');
+            $('header .nav-pills').addClass('blue-links');
+        } else {
+            $('header').removeClass('header-white');
+            $('header .nav-pills').removeClass('blue-links');
+        }
+    }
+
+    /*$(window).scroll(function () {
+        console.log("scroll");
+        $('.region-content .row:not(.footer-content)').each(function (index) {
+            console.log($(this));
+            if ($(this).hasClass('routes') || $(this).hasClass('places')) {
+                $('header').addClass('header-white');
+                $('header .nav-pills').addClass('blue-links');
+            }
+            else {
+                $('header').removeClass('header-white');
+                $('header .nav-pills').removeClass('blue-links');
+            }
+        });
+
+
+    });*/
+
+
 });
