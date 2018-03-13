da03f309805faa4d056394d6f303c814dd065a4c
jaop2316 <jessikita.ldu@gmail.com>
2018-03-12 21:04:49 -0500

    Change english for spanish buttons

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :category
+  validates :name, :presence => true
 
   has_many :placeroutes
   has_many :routes, :through => :placeroute
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,7 +1,15 @@
 class Route < ApplicationRecord
+  validates :name, :presence => true
+  validates :observations, :presence => true
+  validates :horaLV, :presence => true
+  validates :horaS, :presence => true
+  validates :horaDF, :presence => true
+
+
   has_many :stationroutes
   has_many :stations, :through => :stationroute
 
   has_many :placeroutes
   has_many :places, :through => :placeroute
+
 end
